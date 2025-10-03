f748cd717a2b7ed1d57b97b3fa733fc68ed297de
Orlando Jair <orlandojgarciap@gmail.com>
2025-10-02 18:12:54 -0600

    Implementación de modales

--- modules/auth/dashboard.php
+++ modules/auth/dashboard.php
@@ -14,113 +14,140 @@ $user = $_SESSION['usuarioingresando'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FIME Inclusivo</title>
+    <title>Home</title>
     <link rel="stylesheet" href="../../assets/css/sidebar.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+</head>
 </head>
 
 <body>
-    <div class="container">
-        <div class="navigation">
-            <ul>
-                <li>
-                    <a href="#">
-                        <span class="icon">
-                            <ion-icon name="school-outline"></ion-icon>
-                        </span>
-                        <span class="title">FIME Inclusivo</span>
-                    </a>
-                </li>
+        <?php
+    // Obtiene el nombre del archivo de la URL
+    $currentPage = basename($_SERVER['REQUEST_URI']);
+    ?>
+<div class="container">
+    <div class="navigation">
+        <ul>
+            <li>
+                <a href="#">
+                    <span class="icon">
+                        <ion-icon name="school-outline"></ion-icon>
+                    </span>
+                    <span class="title">FIME Inclusivo</span>
+                </a>
+            </li>
 
-                <li class="hovered">
-                    <a href="#">
-                        <span class="icon"><ion-icon name="home-outline"></ion-icon></span>
-                        <span class="title">Inicio</span>
-                    </a>
-                </li>
+            <li class="<?php echo ($currentPage == 'dashboard.php') ? 'active' : ''; ?>">
+                <a href="../../modules/auth/dashboard.php" data-tooltip="Inicio">
+                    <span class="icon"><ion-icon name="home-outline"></ion-icon></span>
+                    <span class="title">Inicio</span>
+                </a>
+            </li>
 
-                <li>
-                    <a href="../../modules/beneficiarios/index_beneficiarios.php">
-                        <span class="icon"><ion-icon name="people-outline"></ion-icon></span>
-                        <span class="title">Beneficiarios</span>
-                    </a>
-                </li>
+            <li class="<?php echo ($currentPage == 'index_beneficiarios.php') ? 'active' : ''; ?>">
+                <a href="../../modules/beneficiarios/index_beneficiarios.php" data-tooltip="Beneficiarios">
+                    <span class="icon"><ion-icon name="people-outline"></ion-icon></span>
+                    <span class="title">Beneficiarios</span>
+                </a>
+            </li>
 
-                <li>
-                    <a href="../../modules/diagnosticos/index_diagnosticos.php">
-                        <span class="icon"><ion-icon name="medkit-outline"></ion-icon></span>
-                        <span class="title">Diagnósticos</span>
-                    </a>
-                </li>
+            <li class="<?php echo ($currentPage == 'index_diagnosticos.php') ? 'active' : ''; ?>">
+                <a href="../../modules/diagnosticos/index_diagnosticos.php" data-tooltip="Diagnósticos">
+                    <span class="icon"><ion-icon name="medkit-outline"></ion-icon></span>
+                    <span class="title">Diagnósticos</span>
+                </a>
+            </li>
 
-                <li>
-                    <a href="../../modules/adaptaciones/index_adaptaciones.php">
-                        <span class="icon"><ion-icon name="construct-outline"></ion-icon></span>
-                        <span class="title">Adaptaciones</span>
-                    </a>
-                </li>
+            <li class="<?php echo ($currentPage == 'index_adaptaciones.php') ? 'active' : ''; ?>">
+                <a href="../../modules/adaptaciones/index_adaptaciones.php" data-tooltip="Adaptaciones">
+                    <span class="icon"><ion-icon name="construct-outline"></ion-icon></span>
+                    <span class="title">Adaptaciones</span>
+                </a>
+            </li>
 
-                <li>
-                    <a href="../../modules/intervenciones/index_intervenciones.php">
-                        <span class="icon"><ion-icon name="clipboard-outline"></ion-icon></span>
-                        <span class="title">Intervenciones</span>
-                    </a>
-                </li>
+            <li class="<?php echo ($currentPage == 'index_intervenciones.php') ? 'active' : ''; ?>">
+                <a href="../../modules/intervenciones/index_intervenciones.php" data-tooltip="Intervenciones">
+                    <span class="icon"><ion-icon name="clipboard-outline"></ion-icon></span>
+                    <span class="title">Intervenciones</span>
+                </a>
+            </li>
 
-                <li>
-                    <a href="../../modules/profesionales/index_profesionales.php">
-                        <span class="icon"><ion-icon name="briefcase-outline"></ion-icon></span>
-                        <span class="title">Profesionales</span>
-                    </a>
-                </li>
+            <li class="<?php echo ($currentPage == 'index_profesionales.php') ? 'active' : ''; ?>">
+                <a href="../../modules/profesionales/index_profesionales.php" data-tooltip="Profesionales">
+                    <span class="icon"><ion-icon name="briefcase-outline"></ion-icon></span>
+                    <span class="title">Profesionales</span>
+                </a>
+            </li>
 
-                <li>
-                    <a href="../../modules/reportes/index_reportes.php">
-                        <span class="icon"><ion-icon name="bar-chart-outline"></ion-icon></span>
-                        <span class="title">Reportes</span>
-                    </a>
-                </li>
+            <li class="<?php echo ($currentPage == 'index_reportes.php') ? 'active' : ''; ?>">
+                <a href="../../modules/reportes/index_reportes.php" data-tooltip="Reportes">
+                    <span class="icon"><ion-icon name="bar-chart-outline"></ion-icon></span>
+                    <span class="title">Reportes</span>
+                </a>
+            </li>
 
-                <li>
-                    <a href="#" onclick="showLogoutModal()">
-                        <span class="icon">
-                            <ion-icon name="log-out-outline"></ion-icon>
-                        </span>
-                        <span class="title">Cerrar Sesión</span>
-                    </a>
-                </li>
-            </ul>
-        </div>
-
-        <div class="main">
-            <div class="topbar">
-                <div class="toggle">
-                    <ion-icon name="menu-outline"></ion-icon>
-                </div>
-                <h2 class="page-title">Programa de Inclusión</h2>
-                <div class="user-box">
-                    <div class="user-info">
-                        <div class="user-name"><?php echo htmlspecialchars($user); ?></div>
-                        <div class="user-role"><?php echo htmlspecialchars($_SESSION['rol']); ?></div>
-                    </div>
-                    <button class="info-btn" onclick="mostrarInfo()">
-                        <ion-icon name="information-outline"></ion-icon>
-                    </button>
-                </div>
+            <li>
+                <a href="#" onclick="showLogoutModal()" data-tooltip="Cerrar Sesión">
+                    <span class="icon">
+                        <ion-icon name="log-out-outline"></ion-icon>
+                    </span>
+                    <span class="title">Cerrar Sesión</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
+    <div class="main">
+        <div class="topbar">
+            <div class="toggle">
+                <ion-icon name="menu-outline"></ion-icon>
             </div>
-            <div class="module-box" id="beneficiarios-box">
-                <div class="content-wrapper">
-                    <div class="text-content">
-                        <h2>N° de Beneficiarios<br><span>Activos: 210</span></h2>
-                        <p>El Programa de Coordinación de Inclusión apoya a beneficiarios mediante estrategias sociales, fomentando igualdad de oportunidades y desarrollo comunitario.</p>
-                        <a href="#" class="btn-consultar">Consultar información</a>
-                    </div>
-                    <div class="image-content">
-                        <img src="../../assets/images/Dibujo_Mujer.png" alt="Dibujo">
-                    </div>
+            <h2 class="page-title">Programa de Inclusión</h2>
+            <div class="user-box">
+                <div class="user-info">
+                    <div class="user-name"><?php echo htmlspecialchars($user); ?></div>
+                    <div class="user-role"><?php echo htmlspecialchars($_SESSION['rol']); ?></div>
+                </div>
+                <button class="info-btn" onclick="mostrarInfo()">
+                    <ion-icon name="information-outline"></ion-icon>
+                </button>
+
+
+            </div>
+        </div>
+        <div class="module-box" id="beneficiarios-box">
+            <div class="content-wrapper">
+                <div class="text-content">
+                    <h2>N° de Beneficiarios<br><span>Activos: 210</span></h2>
+                    <p>El Programa de Coordinación de Inclusión apoya a beneficiarios mediante estrategias sociales, fomentando igualdad de oportunidades y desarrollo comunitario.</p>
+                    <a href="#" class="btn-consultar">Consultar información</a>
+                </div>
+                <div class="image-content">
+                    <img src="../../assets/images/Dibujo_Mujer.png" alt="Dibujo">
                 </div>
             </div>
         </div>
     </div>
+    </div>
+    <!-- Modal de Contacto -->
+    <div id="contactModal" class="modal">
+        <div class="modal-content">
+            <div class="modal-header">
+                <span class="close-btn" id="closeContact">&times;</span>
+                <h2>Información de Contacto</h2>
+            </div>
+            <div class="modal-body">
+                <h3>Orlando Jair - Ingeniero en Sistemas</h3>
+                <p></p>
+                <div class="socialMedia">
+                    <a class="socialIcon" href="https://github.com/MeetsEvil" target="_blank"><i class="fab fa-github"></i></a>
+                    <a class="socialIcon" href="https://www.linkedin.com/in/orlandojgarciap-17a612289/" target="_blank"><i class="fab fa-linkedin"></i></a>
+                    <a class="socialIcon" href="mailto:orlandojgarciap@gmail.com" target="_blank"><i class="fas fa-envelope"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
     <div id="logoutModal" class="modal">
         <div class="modal-content">
@@ -140,35 +167,56 @@ $user = $_SESSION['usuarioingresando'];
 
     <script src="../../assets/js/main.js"></script>
 
+    </script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script src="../../assets/js/main.js"></script>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 
     <script>
-        // Función para mostrar la información del usuario
+        // Modal de Contacto
+        var contactModal = document.getElementById("contactModal");
+        var closeContact = document.getElementById("closeContact");
+        // Seleccionamos el nuevo botón de cancelar
+        var cancelContactBtn = document.getElementById("cancelContactBtn");
+
         function mostrarInfo() {
-            alert("Información del usuario:\nNombre: <?php echo htmlspecialchars($user); ?>\nRol: <?php echo htmlspecialchars($_SESSION['rol']); ?>\nAquí podrías mostrar más detalles en un modal bonito.");
+            contactModal.style.display = "flex";
         }
 
-        // Script para el modal de cerrar sesión
-        var modal = document.getElementById("logoutModal");
-        var span = document.getElementsByClassName("close-btn")[0];
-        var cancelBtn = document.getElementById("cancelBtn");
-
-        function showLogoutModal() {
-            modal.style.display = "flex";
-        }
-
-        span.onclick = function() {
-            modal.style.display = "none";
-        }
-
-        cancelBtn.onclick = function() {
-            modal.style.display = "none";
+        closeContact.onclick = function() {
+            contactModal.style.display = "none";
         }
 
         window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
+            if (event.target == contactModal) {
+                contactModal.style.display = "none";
+            }
+        }
+
+        // Modal de Cerrar Sesión
+        var logoutModal = document.getElementById("logoutModal");
+        var closeLogoutBtn = document.querySelector("#logoutModal .close-btn"); // Selecciona el botón de cerrar del modal de logout
+        var cancelBtn = document.getElementById("cancelBtn");
+
+        function showLogoutModal() {
+            logoutModal.style.display = "flex";
+        }
+
+        // Asegúrate de que los eventos de clic usen las variables correctas
+        closeLogoutBtn.onclick = function() {
+            logoutModal.style.display = "none";
+        }
+
+        cancelBtn.onclick = function() {
+            logoutModal.style.display = "none";
+        }
+
+        // Lógica para cerrar el modal al hacer clic fuera de él
+        window.onclick = function(event) {
+            if (event.target == logoutModal) {
+                logoutModal.style.display = "none";
             }
         }
     </script>
