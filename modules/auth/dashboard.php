336c6ef405e44e7743d93de3ecce2c21cd4117bc
Orlando Jair <orlandojgarciap@gmail.com>
2025-10-27 23:10:51 -0600

    Prueba

--- modules/auth/dashboard.php
+++ modules/auth/dashboard.php
@@ -22,7 +22,7 @@ $user = $_SESSION['usuarioingresando'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Dashboard</title>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="../../assets/css/sidebar.css">
